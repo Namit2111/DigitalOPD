ab881796fafd5d29a365b5cf9f598f7bee417de7
Nmait2111 <namitjain2111@gmail.com>
2025-06-14 11:25:23 +0530

    removed template added a single page

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+// app/index.tsx
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to the OPD Game</Text>
+    </View>
+  );
+}
